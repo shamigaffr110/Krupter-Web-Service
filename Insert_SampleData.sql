7fd509ccd0b8d2d3c6e987774456974341814a24
shamigaffr110 <162407457+shamigaffr110@users.noreply.github.com>
2025-08-19 10:35:35 +0530

    Add files via upload

--- Insert_SampleData.sql
+++ Insert_SampleData.sql
@@ -1,10 +1,7 @@
 
 USE ElectricityBillDB;
 GO
-
 INSERT INTO ElectricityBill (consumer_number, consumer_name, units_consumed, bill_amount) VALUES
-('EB10001', 'Ravi Kumar', 150, 75.0),
-('EB10002', 'Anita Sharma', 350, 700.0),
-('EB10003', 'Suresh Yadav', 620, 1925.0),
-('EB10004', 'Pooja Verma', 1200, 5050.0);
+('EB10001','Ravi Kumar',150,75.0),
+('EB10002','Anita Sharma',350,700.0);
 GO
